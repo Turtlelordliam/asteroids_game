d1fdada63350b8132432962a49dee6ab7aded127
Turtlelordliam <liammichaelgraham@gmail.com>
2025-12-14 12:22:58 +1100

    player object created

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-
+from player import Player
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock1 = pygame.time.Clock()
+    dt = 0
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -14,7 +20,9 @@ def main():
                 return
         
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
+        dt = clock1.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
